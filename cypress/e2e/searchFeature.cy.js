c033855938561289e897c22933c3075d44ee3c89
‘thesumyatmaw’ <thesumyatmaw@gmail.com>
2022-11-03 17:32:06 +0100

    search by invalid keywords and some letters

--- cypress/e2e/searchFeature.cy.js
+++ cypress/e2e/searchFeature.cy.js
@@ -1,4 +1,5 @@
 describe('Search functions for IMDB', () => {
+  // I use "before" to run once before all the testcases
   before(() => {
     cy.visit('/')
   })
@@ -24,5 +25,35 @@ describe('Search functions for IMDB', () => {
         cy.log($li.text())
         expect($li.text()).to.be.oneOf(categories)
       })
+    // To close search categories list, I clicked again.
+    cy.get('.ipc-button[for="navbar-search-category-select"]').click()
+  })
+
+  it('Type invalid keyword on the searchbar & check error message', () => {
+    cy.get('#suggestion-search')
+    cy.get('input[type="text"]').type('%$9')
+    cy.get('div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]')
+    cy.get('.ipc-error-message').should('have.text', 'No results found.')
+  })
+
+  it('Search by some letters on the search bar ', () => {
+    cy.intercept(
+      'GET',
+      'https://v3.sg.media-imdb.com/suggestion/x/spi.json?includeVideos=1'
+    ).as('getmovielists')
+
+    cy.get('#suggestion-search')
+    cy.get('input[type="text"]').clear().type('Spi')
+    cy.wait('@getmovielists').its('response.statusCode').should('eq', 200)
+    cy.get('@getmovielists').then((resultlists) => {
+      cy.log(resultlists)
+      const resultmovieslist = resultlists.response.body.d
+      resultmovieslist.forEach((Movielists) => {
+        cy.log(Movielists.l)
+        cy.get(
+          'div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]'
+        ).contains(Movielists.l)
+      })
+    })
   })
 })

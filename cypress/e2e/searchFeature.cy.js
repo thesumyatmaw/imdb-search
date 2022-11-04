73f28799fc6857abb946c3cc2458df162bacaceb
‘thesumyatmaw’ <thesumyatmaw@gmail.com>
2022-11-04 16:14:57 +0100

    search by TV Episode

--- cypress/e2e/searchFeature.cy.js
+++ cypress/e2e/searchFeature.cy.js
@@ -1,9 +1,10 @@
 describe('Search functions Test Suite for IMDB', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
   describe('Search by Movie names Test Cases', () => {
     // I use "before" to run once before all the testcases
-    before(() => {
-      cy.visit('/')
-    })
 
     it('Go to the IMDB page and assert the search bar & search menu items', () => {
       const categories = [
@@ -78,11 +79,39 @@ describe('Search functions Test Suite for IMDB', () => {
           .contains(getmovie.l)
           .click()
 
-        cy.get('h1[data-testid="hero-title-block__title"]').should(
-          'have.text',
-          getmovie.l
-        )
+        cy.get('h1[data-testid="hero-title-block__title"]', {
+          timeout: 1000,
+        }).should('have.text', getmovie.l)
       })
+    })
+  })
+
+  //Search by TV Episodesfrom search menus that doesn't work on auto Complete in IMDB.
+  //So, to display TV Episodes search result I need to use Enter keyword.
+  //Technical stuck : I cannot use cypress intercept.Because I didn't see network api call (Xhr/fetch) when I searched.
+  describe('Search by TV Episodes Test Case', () => {
+    it('Select "TV Episodies" from search menu & search TV episodes', () => {
+      cy.get('#home_img_holder')
+      cy.get('#nav-search-form')
+      cy.get('.ipc-button[for="navbar-search-category-select"]').click()
+      cy.get('.ipc-menu[role="presentation"]')
+      cy.get('ul.searchCatSelector')
+        .find('li[role="menuitem"]')
+        .eq(2)
+        .should('contain', 'TV Episodes')
+        .click()
+      cy.get('.ipc-button[for="navbar-search-category-select"]').should(
+        'have.attr',
+        'aria-label',
+        'TV Episodes'
+      )
+      cy.get('input[type="text"]').type('Stranger Things{enter}')
+      //Only check search result on the first row.
+      cy.get('.findSection', { timeout: 5000 })
+      cy.get('table.findList')
+        .find('tbody>tr>td.result_text')
+        .eq(0)
+        .should('contain', 'Stranger Things')
     })
   })
 })

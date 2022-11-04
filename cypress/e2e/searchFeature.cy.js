672ff325b506fe58c33e94e5b757fc47f199853a
‘thesumyatmaw’ <thesumyatmaw@gmail.com>
2022-11-04 18:21:53 +0100

    search by Celeb name

--- cypress/e2e/searchFeature.cy.js
+++ cypress/e2e/searchFeature.cy.js
@@ -1,7 +1,6 @@
 describe('Search functions Test Suite for IMDB', () => {
-  // I use "before" to run once before all the testcases
-
-  describe('Search by Movie names Test Cases', () => {
+  describe('Search by Movie name Test Cases', () => {
+    // I use "before" to run once before all the testcases
     before(() => {
       cy.visit('/')
     })
@@ -89,7 +88,7 @@ describe('Search functions Test Suite for IMDB', () => {
   //Search by TV Episodes from search menus that doesn't work on auto Complete in IMDB.
   //So, to display TV Episodes search result I need to use Enter keyword.
   //Technical stuck : I cannot use cypress intercept.Because I didn't see network api call (Xhr/fetch) when I searched.
-  describe('Search by TV Episodes Test Case', () => {
+  describe('Search by TV Episode Test Case', () => {
     before(() => {
       cy.visit('/')
     })
@@ -120,11 +119,50 @@ describe('Search functions Test Suite for IMDB', () => {
     })
   })
 
-  describe('Search by Celebs Test Case', () => {
+  describe('Search by Celeb Test Case', () => {
     before(() => {
       cy.visit('/')
     })
 
-    it('Select "Celebs" from search menu & search Cele person', () => {})
+    it('Select "Celebs" from search menu & search Cele person', () => {
+      cy.intercept(
+        'GET',
+        'https://v3.sg.media-imdb.com/suggestion/names/x/jennifer%20lawrence.json?includeVideos=1'
+      ).as('getceleb')
+
+      cy.get('#home_img_holder')
+      cy.get('#nav-search-form')
+      cy.get('.ipc-button[for="navbar-search-category-select"]').click()
+      cy.get('.ipc-menu[role="presentation"]')
+      cy.get('ul.searchCatSelector')
+        .find('li[role="menuitem"]')
+        .eq(3)
+        .should('contain', 'Celebs')
+        .click()
+      cy.get('.ipc-button[for="navbar-search-category-select"]').should(
+        'have.attr',
+        'aria-label',
+        'Celebs'
+      )
+      cy.get('input[type="text"]').type('Jennifer Lawrence')
+      cy.wait('@getceleb').its('response.statusCode').should('eq', 200)
+      cy.get('@getceleb').then((resultceleb) => {
+        cy.log(resultceleb)
+        console.log(resultceleb)
+        const getceleblists = resultceleb.response.body.d
+        const celebname = 'Jennifer Lawrence'
+        const getceleb = getceleblists.find(
+          (resultname) => resultname.l === celebname
+        )
+        cy.log(getceleb.l)
+        cy.get('div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]')
+          .contains(getceleb.l)
+          .click()
+
+        cy.get('h1', {
+          timeout: 3000,
+        }).should('contain', getceleb.l)
+      })
+    })
   })
 })

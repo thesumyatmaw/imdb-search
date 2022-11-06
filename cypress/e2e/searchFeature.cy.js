4f3a6d06f0befb1a79650250b0a3720b19db3200
‘thesumyatmaw’ <thesumyatmaw@gmail.com>
2022-11-06 01:45:17 +0100

    refactor and finalize

--- cypress/e2e/searchFeature.cy.js
+++ cypress/e2e/searchFeature.cy.js
@@ -1,5 +1,5 @@
-describe('Search functions Test Suite for IMDB', () => {
-  describe('Search by Movie name Test Cases', () => {
+describe('Search functions for IMDB', () => {
+  describe('Search by Movie name', () => {
     beforeEach(() => {
       cy.visit('/')
     })
@@ -21,68 +21,68 @@ describe('Search functions Test Suite for IMDB', () => {
       cy.get('ul.searchCatSelector')
         .children('li[role="menuitem"]')
         .each(($li) => {
-          console.log($li)
-          cy.log($li.text())
           expect($li.text()).to.be.oneOf(categories)
         })
     })
 
     it('Type invalid keyword on the searchbar & check error message', () => {
+      const searchKeyword = '%$9'
+      const errorMsg = 'No results found.'
       cy.get('#suggestion-search')
-      cy.get('input[type="text"]').type('%$9')
+      cy.get('input[type="text"]').type(searchKeyword)
       cy.get('div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]')
-      cy.get('.ipc-error-message').should('have.text', 'No results found.')
+      cy.get('.ipc-error-message').should('have.text', errorMsg)
     })
 
     it('Search by some letters on the search bar ', () => {
+      const searchKeyword = 'spi'
       cy.intercept(
         'GET',
         'https://v3.sg.media-imdb.com/suggestion/x/spi.json?includeVideos=1'
       ).as('getmovielists')
 
       cy.get('#suggestion-search')
-      cy.get('input[type="text"]').clear().type('Spi')
+      cy.get('input[type="text"]').clear().type(searchKeyword)
       cy.wait('@getmovielists').its('response.statusCode').should('eq', 200)
-      cy.get('@getmovielists').then((resultlists) => {
-        cy.log(resultlists)
-        const resultmovieslist = resultlists.response.body.d
-        resultmovieslist.forEach((Movielists) => {
-          cy.log(Movielists.l)
+      cy.get('@getmovielists').then((result) => {
+        const movies = result.response.body.d
+        movies.forEach((movie) => {
           cy.get(
             'div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]'
-          ).contains(Movielists.l)
+          ).then(($searchResult) => {
+            const wrappedResult = cy.wrap($searchResult)
+            wrappedResult.should('contain', searchKeyword)
+            wrappedResult.should('contain', movie.l)
+          })
         })
       })
     })
 
     it('Search movie name on the search bar & click on this movie', () => {
+      const searchKeyword = 'Spirited Away'
       cy.intercept(
         'GET',
         'https://v3.sg.media-imdb.com/suggestion/x/spirited%20away.json?includeVideos=1'
-      ).as('getmovie')
+      ).as('getMovie')
       cy.get('#suggestion-search')
-      cy.get('input[type="text"]').clear().type('Spirited Away')
-      cy.wait('@getmovie').its('response.statusCode').should('eq', 200)
-      cy.get('@getmovie').then((results) => {
-        cy.log(results)
-        const getmovieslist = results.response.body.d
-        const moviename = 'Spirited Away'
-        const getmovie = getmovieslist.find(
-          (resultmoviename) => resultmoviename.l === moviename
-        )
-        cy.log(getmovie.l)
+      cy.get('input[type="text"]').clear().type(searchKeyword)
+      cy.wait('@getMovie').its('response.statusCode').should('eq', 200)
+      cy.get('@getMovie').then((result) => {
+        const movies = result.response.body.d
+
+        const foundMovie = movies.find((movie) => movie.l === searchKeyword)
         cy.get('div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]')
-          .contains(getmovie.l)
+          .contains(foundMovie.l)
           .click()
 
         cy.get('h1[data-testid="hero-title-block__title"]', {
           timeout: 1000,
-        }).should('have.text', getmovie.l)
+        }).should('have.text', foundMovie.l)
       })
     })
   })
 
-  describe('Search by TV Episode Test Case', () => {
+  describe('Search by TV Episode', () => {
     beforeEach(() => {
       cy.visit('/')
     })
@@ -90,6 +90,8 @@ describe('Search functions Test Suite for IMDB', () => {
     it('Select "TV Episodies" from search menu & search TV episodes', () => {
       // Technical limitation : Cannot use cypress intercept in this test case.
       // Because IMDB use document type server request instead of Xhr api network call.
+
+      const searchKeyword = 'Manifest'
       cy.get('#home_img_holder')
       cy.get('#nav-search-form')
       cy.get('.ipc-button[for="navbar-search-category-select"]').click()
@@ -104,7 +106,7 @@ describe('Search functions Test Suite for IMDB', () => {
         'aria-label',
         'TV Episodes'
       )
-      cy.get('input[type="text"]').type('Manifest{enter}')
+      cy.get('input[type="text"]').type(`${searchKeyword}{enter}`)
       // Assert the search result for the first row.
       cy.get('body').then(($body) => {
         if ($body.find('div[id="main"]').length > 0) {
@@ -112,24 +114,25 @@ describe('Search functions Test Suite for IMDB', () => {
           cy.get('table.findList')
             .find('tbody>tr>td.result_text')
             .eq(0)
-            .should('contain', 'Manifest')
+            .should('contain', searchKeyword)
         } else {
           cy.get('section[data-testid="find-results-section-title"]')
           cy.get('ul.ipc-metadata-list')
             .find('li')
             .eq(0)
-            .should('contain', 'Manifest')
+            .should('contain', searchKeyword)
         }
       })
     })
   })
 
-  describe('Search by Celeb Test Case', () => {
+  describe('Search by Celeb', () => {
     beforeEach(() => {
       cy.visit('/')
     })
 
     it('Select "Celebs" from search menu & search Cele person', () => {
+      const searchKeyword = 'Jennifer Lawrence'
       cy.intercept(
         'GET',
         'https://v3.sg.media-imdb.com/suggestion/names/x/jennifer%20lawrence.json?includeVideos=1'
@@ -149,29 +152,23 @@ describe('Search functions Test Suite for IMDB', () => {
         'aria-label',
         'Celebs'
       )
-      cy.get('input[type="text"]').type('Jennifer Lawrence')
+      cy.get('input[type="text"]').type(searchKeyword)
       cy.wait('@getceleb').its('response.statusCode').should('eq', 200)
-      cy.get('@getceleb').then((resultceleb) => {
-        cy.log(resultceleb)
-        console.log(resultceleb)
-        const getceleblists = resultceleb.response.body.d
-        const celebname = 'Jennifer Lawrence'
-        const getceleb = getceleblists.find(
-          (resultname) => resultname.l === celebname
-        )
-        cy.log(getceleb.l)
+      cy.get('@getceleb').then((result) => {
+        const celebs = result.response.body.d
+        const foundCeleb = celebs.find((celeb) => celeb.l === searchKeyword)
         cy.get('div[class="sc-90ef1f68-0 lyCuq imdb-header__search-menu"]')
-          .contains(getceleb.l)
+          .contains(foundCeleb.l)
           .click()
 
         cy.get('h1', {
           timeout: 3000,
-        }).should('contain', getceleb.l)
+        }).should('contain', foundCeleb.l)
       })
     })
   })
 
-  describe('Search by Companies Test Case', () => {
+  describe('Search by Companies', () => {
     beforeEach(() => {
       cy.visit('/')
     })
@@ -179,6 +176,7 @@ describe('Search functions Test Suite for IMDB', () => {
       // Technical limitation : Cannot use cypress intercept in this test case too.
       // Because IMDB use document type server request instead of Xhr api network call.
 
+      const searchKeyword = 'Marvel Studios'
       cy.get('#home_img_holder')
       cy.get('#nav-search-form')
       cy.get('.ipc-button[for="navbar-search-category-select"]').click()
@@ -193,7 +191,7 @@ describe('Search functions Test Suite for IMDB', () => {
         'aria-label',
         'Companies'
       )
-      cy.get('input[type="text"]').type('Marvel Studios{enter}')
+      cy.get('input[type="text"]').type(`${searchKeyword}{enter}`)
 
       // Assert the search result for the first row.
       cy.get('body').then(($body) => {
@@ -202,13 +200,13 @@ describe('Search functions Test Suite for IMDB', () => {
           cy.get('table.findList')
             .find('tbody>tr>td.result_text')
             .eq(0)
-            .should('contain', 'Marvel Studios')
+            .should('contain', searchKeyword)
         } else {
           cy.get('section[data-testid="find-results-section-company"]')
           cy.get('ul.ipc-metadata-list')
             .find('li')
             .eq(0)
-            .should('contain', 'Marvel Studios')
+            .should('contain', searchKeyword)
         }
       })
     })
